db15741ae06efb13a55de2a75a8179dc65d3fa81
mr-m-apps <suppo.onion@gmail.com>
2026-06-22 11:25:45 +0300

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,29 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    'bot/index': 'src/bot/index.ts',
-    'cdn/index': 'src/cdn/index.ts',
-    'dev/index': 'src/dev/index.ts',
-    'core/index': 'src/core/index.ts',
-    browser: 'src/browser.ts',
+export default defineConfig([
+  {
+    entry: {
+      index: 'src/index.ts',
+      'bot/index': 'src/bot/index.ts',
+      'cdn/index': 'src/cdn/index.ts',
+      'dev/index': 'src/dev/index.ts',
+      'core/index': 'src/core/index.ts',
+    },
+    format: ['cjs', 'esm'],
+    dts: true,
+    external: ['react', 'react-dom'],
+    clean: true,
+    treeshake: true,
+    target: 'es2017',
   },
-  format: ['cjs', 'esm'],
-  dts: true,
-  external: ['react', 'react-dom'],
-  clean: true,
-  treeshake: true,
-  target: 'es2017',
-});
+  {
+    entry: { browser: 'src/browser.ts' },
+    format: ['iife'],
+    globalName: 'TgKit',
+    dts: false,
+    clean: false,
+    treeshake: true,
+    target: 'es2017',
+    minify: true,
+  },
+]);
